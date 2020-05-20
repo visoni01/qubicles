568aa3544834dc2960f46ebbb2325562ff696170
Nitin Verma <nverma@qubicles.io>
2020-05-21 01:20:57 +0530

    Made changes to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bulma/css/bulma.css'
 import './styles/index.scss'
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -13,7 +14,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
