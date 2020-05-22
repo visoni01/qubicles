8cc2871a0aec1ba8b888885526647876bd7b9898
Nitin Verma <nverma@qubicles.io>
2020-05-22 20:13:50 +0530

    Changed imports

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,17 @@ import React from "react";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import Signup from "./container/User/Signup/";
+import Signup from "./container/User/Signup/SignUp";
 import store from "./redux-saga/store";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route exact path="/signup" component={Signup} />
-        </Switch>
-      </Router>
-    </Provider>
-  );
-}
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route exact path="/signup" component={Signup} />
+      </Switch>
+    </Router>
+  </Provider>
+);
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bulma/css/bulma.css'
-import './styles/index.scss'
+// import './styles/index.scss'
 
 
 ReactDOM.render(
