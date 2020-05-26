a0c2c4486aed0597c4c0e684e0bcbae7eec7ed68
Nitin Verma <nverma@qubicles.io>
2020-05-26 10:17:12 +0530

    Added circular loader in the email verificaion.

--- frontend/src/container/User/Signup/EmailVerification/index.jsx
+++ frontend/src/container/User/Signup/EmailVerification/index.jsx
@@ -1,22 +1,26 @@
-import React, { useEffect } from "react"
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { emailVerificationStart } from "../../../../redux-saga/redux/emailVerification";
+import { CircularLoader } from "../../../../components/loaders";
+import "./style.scss";
 
 const EmailVerification = () => {
-  const { token } = useParams()
-    const dispatch = useDispatch()
-    useEffect(() => {
-      dispatch(emailVerificationStart(token))
-    }, [])
-    const { error, isLoading, success } = useSelector((state) => state.emailVerification);
-    return (
-      <>
-      {isLoading && ( <> Please wait</>)}
-      {success && ( <> You're email has been verified </>)}
-      {error && ( <> An unexpected error occured </>)}
-      </>
-    )
-}
+  const { token } = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(emailVerificationStart(token));
+  }, []);
+  const { error, isLoading, success } = useSelector(
+    (state) => state.emailVerification
+  );
+  return (
+    <>
+      <CircularLoader isLoading={isLoading} optionalStyle={"clip-loader-css"} />
+      {success && <> You're email has been verified </>}
+      {error && <> An unexpected error occured </>}
+    </>
+  );
+};
 
-export default EmailVerification
+export default EmailVerification;
