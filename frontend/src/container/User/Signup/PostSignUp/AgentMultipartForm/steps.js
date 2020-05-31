fc9aff37d55cdac168c281475a679c1ab9c19015
Nitin Verma <nverma@qubicles.io>
2020-05-31 11:55:32 +0530

    Defined fields for step 2.

--- frontend/src/container/User/Signup/PostSignUp/AgentMultipartForm/steps.js
+++ frontend/src/container/User/Signup/PostSignUp/AgentMultipartForm/steps.js
@@ -2,15 +2,33 @@ import * as yup from 'yup'
 
 const steps = {
   1: {
-    fields: [ [ 'Date of Birth', 'date', 'birth_date' ], [ 'Gender', 'checkbox', 'gender', [
-      [ 'Male', 'male', 'Male' ], [ 'Female', 'female', 'Female' ], [ 'Others', 'others', 'Others' ],
-    ] ], [ 'SSN', 'text', 'ssn' ] ],
-    schema: yup.object().shape( {
-      birth_date: yup.string().required( '*Required' ),
-      ssn: yup.string().required( '*Required' ),
+    fields: [['Date of Birth', 'date', 'birth_date'], ['Gender', 'checkbox', 'gender', [
+      ['Male', 'male', 'Male'], ['Female', 'female', 'Female'], ['Others', 'others', 'Others'],
+    ]], ['SSN', 'text', 'ssn']],
+    schema: yup.object().shape({
+      birth_date: yup.string().required('*Required'),
+      ssn: yup.string().required('*Required'),
       gender: yup.bool(),
-    } ),
-  }
+    }),
+  },
+  2: {
+    fields: [
+      ['Street Address', 'text', 'street_address'],
+      ['City', 'text', 'city'],
+      ['State', 'text', 'state'],
+      ['Zip', 'text', 'zip'],
+      ['Home Phone', 'text', 'home_phone'],
+      ['Mobile Phone', 'text', 'mobile_phone']
+    ],
+    schema: yup.object().shape({
+      street_address: yup.string(),
+      city: yup.string(),
+      state: yup.string(),
+      zip: yup.string(),
+      home_phone: yup.string(),
+      mobile_phone: yup.string(),
+    }),
+  },
 }
 
 export default steps
