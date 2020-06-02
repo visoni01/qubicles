3a5f97d698a9ca65ad379bdf00f38fef2cea050b
Nitin Verma <nverma@qubicles.io>
2020-06-02 17:30:37 +0530

    Added stepper in the agent sign up form.

--- frontend/src/container/User/Signup/PostSignUp/AgentMultipartForm/index.jsx
+++ frontend/src/container/User/Signup/PostSignUp/AgentMultipartForm/index.jsx
@@ -1,12 +1,30 @@
 import React, { useState } from 'react'
-import StepForm from './stepForm'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
+
+import StepperComponent from '../../../../../components/Stepper'
+import MutlipartForm from './multipartForm'
 
 const AgentMultipartForm = () => {
   const [ step, setStep ] = useState( 1 )
   const handleOnNext = () => setStep( step + 1 )
   const handleOnBack = () => setStep( step - 1 )
 
-  return <StepForm step={ step } onNext={ handleOnNext } onBack={ handleOnBack } />
+  const steps = [
+    { icon: faCog },
+    { icon: faCog },
+    { icon: faCog },
+    { icon: faCog },
+    { icon: faCog },
+    { icon: faCog },
+    { icon: faCog },
+  ]
+
+  return (
+    <>
+      <StepperComponent steps={ steps } activeStep={ step } />
+      <MutlipartForm step={ step } onNext={ handleOnNext } onBack={ handleOnBack } />
+    </>
+  )
 }
 
 export default AgentMultipartForm
