1181f0d7bb9f2618dff2ead389600c3b45e58902
Nitin Verma <nverma@qubicles.io>
2020-06-03 08:30:20 +0530

    Added stepper in the contact center sign up form.

--- frontend/src/container/User/Signup/PostSignUp/ContactCenterMultipartForm/index.jsx
+++ frontend/src/container/User/Signup/PostSignUp/ContactCenterMultipartForm/index.jsx
@@ -1,12 +1,27 @@
 import React, { useState } from 'react'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
+
 import Form from './form'
+import StepperComponent from '../../../../../components/Stepper'
 
 const ContactCenterMultiPartForm = () => {
   const [ step, setStep ] = useState( 1 )
   const handleOnNext = () => setStep( step + 1 )
   const handleOnBack = () => setStep( step - 1 )
 
-  return <Form step={ step } onNext={ handleOnNext } onBack={ handleOnBack } />
+  const steps = [
+    { icon: faCog },
+    { icon: faCog },
+    { icon: faCog },
+    { icon: faCog },
+  ]
+
+  return (
+    <>
+      <StepperComponent steps={ steps } activeStep={ step } />
+      <Form step={ step } onNext={ handleOnNext } onBack={ handleOnBack } />
+    </>
+  )
 }
 
 export default ContactCenterMultiPartForm
