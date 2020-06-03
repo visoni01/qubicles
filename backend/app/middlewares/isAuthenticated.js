4381b7b3dfa0ac8d958aa3bd8cc74c94c650cb59
Shivam Jaiswal <sjaiswal@qubicles.io>
2020-06-03 20:54:33 +0530

    Added middleware for secure route

---
+++ backend/app/middlewares/isAuthenticated.js
@@ -0,0 +1,16 @@
+import passport from 'passport'
+
+export function isAuthenticated (req, res, next) {
+  passport.authenticate('jwt', (err, user) => {
+    if (err) {
+      return res.status(401).json({ message: err })
+    }
+    req.login(user, loginErr => {
+      if (loginErr) {
+        return res.status(401).send(loginErr)
+      }
+      req.body.user_id = user.user_id
+      next()
+    })
+  })(req, res, next)
+}
--- backend/routes/api/v1/userEmployer.router.js
+++ backend/routes/api/v1/userEmployer.router.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import userEmployerController from '../../../app/controllers/userEmployer.controller'
+import { isAuthenticated } from './../../../app/middlewares/isAuthenticated'
 
 const args = { mergeParams: true }
 const userEmployerRouter = express.Router(args)
 
 userEmployerRouter.route('/postSignup/:step')
-  .post(userEmployerController.postSignupEmployer)
+  .post(isAuthenticated, userEmployerController.postSignupEmployer)
 
 export { userEmployerRouter }
