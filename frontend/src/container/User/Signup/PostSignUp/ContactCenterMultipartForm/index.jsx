19b5adbceac8529a957a0cf61c39e664e5df862d
Nitin Verma <nverma@qubicles.io>
2020-06-04 18:06:16 +0530

    Added contact form data to reducer.

--- frontend/src/container/User/Signup/PostSignUp/ContactCenterMultipartForm/index.jsx
+++ frontend/src/container/User/Signup/PostSignUp/ContactCenterMultipartForm/index.jsx
@@ -4,34 +4,33 @@ import { faCog } from '@fortawesome/free-solid-svg-icons'
 import { useSelector, useDispatch } from 'react-redux'
 import Form from './multipartForm'
 import StepperComponent from '../../../../../components/Stepper'
-import { postSignUpStepStart } from '../../../../../redux-saga/redux/postSignup'
+import { postSignUpStepStart, handleBackStep } from '../../../../../redux-saga/redux/postSignup'
 
 const ContactCenterMultiPartForm = ( { role } ) => {
   const dispatch = useDispatch()
   const {
-    error, isLoading, success, stepsData,
+    stepsData, currentStep,
   } = useSelector(
     ( state ) => state.postSignUp,
   )
 
-  const [ step, setStep ] = useState( 1 )
   const handleOnNext = ( data ) => {
-    dispatch( postSignUpStepStart( { type: role, step, data } ) )
-    success && setStep( step + 1 )
+    dispatch( postSignUpStepStart( { type: role, currentStep, data } ) )
   }
-  const handleOnBack = () => setStep( step - 1 )
+  const handleOnBack = () => dispatch( handleBackStep() )
   const handleSubmit = () => {}
 
   const steps = [ { icon: faCog }, { icon: faCog }, { icon: faCog } ]
 
   return (
     <>
-      <StepperComponent steps={ steps } activeStep={ step - 1 } />
+      <StepperComponent steps={ steps } activeStep={ currentStep - 1 } />
       <Form
-        step={ step }
+        step={ currentStep }
         onNext={ handleOnNext }
         onBack={ handleOnBack }
         onSubmit={ handleSubmit }
+        stepData={ stepsData && stepsData[ currentStep ] }
       />
     </>
   )
