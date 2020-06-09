8b86feb4bbd3ff8421937949cc9692ff8ee35a86
Nitin Verma <nverma@qubicles.io>
2020-06-09 18:22:35 +0530

    Basic set up of the Invite page.

---
+++ frontend/src/container/InviteFriendsPage/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './style.scss'
+
+const InviteFriends = () => {
+  return <div>
+    <div className="background-image-div">
+      <i />
+      <h1 className="div1-h1">Invite Friends & <br />
+        Earn Up To $5 and Free Crypto
+      </h1>
+      <h3 className="div1-h2">Introduce your friends, earn free credits and tokens</h3>
+    </div>
+  </div>
+}
+
+export default InviteFriends
