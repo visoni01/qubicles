5882ad71c277c33936c3699487d7b7f9753724e7
Nitin Verma <nverma@qubicles.io>
2020-06-10 16:35:04 +0530

    Register watcher and reducer for invite page.

--- frontend/src/redux-saga/redux/index.js
+++ frontend/src/redux-saga/redux/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from '@reduxjs/toolkit'
 import signupReducer from './signup'
 import emailVerificationReducer from './emailVerification'
 import postSignUpReducer from './postSignup'
+import invitePageReducer from './invitePage'
 
 const rootReducer = combineReducers( {
   signup: signupReducer,
   emailVerification: emailVerificationReducer,
   postSignUp: postSignUpReducer,
+  invitePage: invitePageReducer,
 } )
 
 export default rootReducer
