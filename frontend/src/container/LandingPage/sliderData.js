87e5afdf7f698b639693b94de0ec54d16edf6476
Nitin Verma <nverma@qubicles.io>
2020-06-23 16:21:08 +0530

    Added slides data.

---
+++ frontend/src/container/LandingPage/sliderData.js
@@ -0,0 +1,32 @@
+const sliderData = [
+  {
+    heading: "World's first contact center powered by blockchain",
+    detail: `Where talented customer service, sales and support agents
+      connect directly with contact centers for work. No middlemen involved.`,
+    buttonName: 'Sign Up for Free',
+    dotName: 'contact center powered by blockchain',
+  },
+  {
+    heading: 'Run your contact center from anywhere in the world',
+    detail: `Welcome to the future - where you are the boss with a more flexible schedule and working
+    from the comfort of your own home.`,
+    buttonName: 'Sign Up for Free',
+    dotName: 'earn qbe tokens',
+  },
+  {
+    heading: 'Earn Qubicle cryptocurrency rewards for a job well done',
+    detail: `We utilize blockchain technology to create a unique reward-based economy,
+      where users earn cryptocurrency for meeting program KPIs.`,
+    buttonName: 'Sign Up for Free',
+    dotName: 'work at home contact center',
+  },
+  {
+    heading: 'All-in-one cloud contact center software at your fingertip',
+    detail: `Designed to meet the needs of small, mid-sized, and enterprise contact centers alike, we scale as your
+      business grows.`,
+    buttonName: 'Sign Up for Free',
+    dotName: 'all-in-one contact center software',
+  },
+]
+
+export default sliderData
