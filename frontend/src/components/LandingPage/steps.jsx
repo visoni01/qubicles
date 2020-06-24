200fb9f78c7dd932d416f65752115516d7f783d6
Nitin Verma <nverma@qubicles.io>
2020-06-24 17:31:19 +0530

    Step 7 as component.

--- frontend/src/components/LandingPage/steps.jsx
+++ frontend/src/components/LandingPage/steps.jsx
@@ -525,6 +525,123 @@ const Steps = ({ history }) => (
       </div>
     </section>
     {/* /Support cards section */}
+    {/* Static Testimonials */}
+    <section
+      id="card-testimonials"
+      className="section parallax is-relative is-medium"
+      data-background
+      data-color="#000"
+      data-color-opacity={ 0.0 }
+    >
+      <div className="container">
+        {/* Title */}
+        <div className="section-title-wrapper">
+          <div className="bg-number">7</div>
+          <h2 className="title section-title has-text-centered dark-text">
+            Our clients love us
+          </h2>
+          <div className="subtitle has-text-centered is-tablet-padded">
+            Wondering what it's like to work with us? Just hear what some our
+            valued customers have to say.
+          </div>
+        </div>
+        <div className="content-wrapper">
+          <div className="columns is-vcentered">
+            <div className="column" />
+            <div className="column is-10">
+              {/* Testimonials */}
+              <div className="columns is-vcentered">
+                <div className="column is-6">
+                  {/* Testimonial item */}
+                  <div className="flex-card testimonial-card light-bordered light-raised padding-20 mb-20">
+                    <div className="testimonial-title">
+                      My experience has been great
+                    </div>
+                    <div className="testimonial-text">
+                      Overall my experience has been great. I have found a
+                      solution which is functional, priced well, with great
+                      customer service. Exactly what I was looking for.
+                    </div>
+                    <div className="user-id">
+                      <img className src="img/icons/chat-user.png" alt="" />
+                      <div className="info">
+                        <div className="name">Jennifer Thorne</div>
+                        <div className="position">COO</div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* Testimonial item */}
+                  <div className="flex-card testimonial-card light-bordered light-raised padding-20">
+                    <div className="testimonial-title">User friendly UI</div>
+                    <div className="testimonial-text">
+                      Per minute billing and easy to set up. User friendly UI,
+                      takes just one sys-admin on our end to manage the
+                      software.
+                    </div>
+                    <div className="user-id">
+                      <img className src="img/icons/chat-user.png" alt="" />
+                      <div className="info">
+                        <div className="name">Abhishek Verma</div>
+                        <div className="position">IT Manager</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="column is-6">
+                  {/* Testimonial item */}
+                  <div className="flex-card testimonial-card light-bordered light-raised padding-20 mb-20">
+                    <div className="testimonial-title">
+                      Easy to use and very affordable
+                    </div>
+                    <div className="testimonial-text">
+                      It is very easy to use like a, b, c.. Very user
+                      friendly. It was easy to use and very affordable.
+                    </div>
+                    <div className="user-id">
+                      <img className src="img/icons/chat-user.png" alt="" />
+                      <div className="info">
+                        <div className="name">Bella Martin</div>
+                        <div className="position">Appointment Setter</div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* Testimonial item */}
+                  <div className="flex-card testimonial-card light-bordered light-raised padding-20">
+                    <div className="testimonial-title">
+                      The support is awesome!
+                    </div>
+                    <div className="testimonial-text">
+                      The support is awesome! Easy to navigate, has self help
+                      knowledge base, real time reporting, easy to create
+                      stations and users, ...everything!
+                    </div>
+                    <div className="user-id">
+                      <img className src="img/icons/chat-user.png" alt="" />
+                      <div className="info">
+                        <div className="name">Carlo Angelo Pablo</div>
+                        <div className="position">Sr. Project Manager</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* /Testimonials */}
+            </div>
+            <div className="column" />
+          </div>
+        </div>
+        <h2 className="title has-text-centered is-title-reveal pt-80">
+          <button
+            type="button"
+            className="button button-cta btn-align rounded raised primary-btn steps-button-color"
+            onClick={ () => history.push('/signup') }
+          >
+            Get Started for Free
+          </button>
+        </h2>
+      </div>
+    </section>
+    {/* /Static Testimonials */}
   </>
 )
 
