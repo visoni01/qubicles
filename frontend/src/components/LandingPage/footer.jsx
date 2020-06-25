fd6cd830c8816eaa2cf11f72d42f7ea62ec71599
Nitin Verma <nverma@qubicles.io>
2020-06-25 16:06:06 +0530

    Added icons and links in the footer.

--- frontend/src/components/LandingPage/footer.jsx
+++ frontend/src/components/LandingPage/footer.jsx
@@ -1,4 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {
+  FacebookIcon,
+  LinkedinIcon,
+  TwitterIcon,
+} from 'react-share'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopyright } from '@fortawesome/free-solid-svg-icons'
+import { qbeDark } from '../../assets/images/landingPage'
 
 const Footer = () => (
   <footer className='footer footer-dark'>
@@ -10,9 +19,9 @@ const Footer = () => (
               <h3>Product</h3>
             </div>
             <ul className='link-list'>
-              <li><a href='/'>Home</a></li>
-              <li><a href='/agents'>For Agents</a></li>
-              <li><a href='/contactcenters'>For Contact Centers</a></li>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/agents'>For Agents</Link></li>
+              <li><Link to='/contactcenters'>For Contact Centers</Link></li>
             </ul>
           </div>
         </div>
@@ -22,12 +31,22 @@ const Footer = () => (
               <h3>Company</h3>
             </div>
             <ul className='link-list'>
-              <li><a href='/about'>About</a></li>
-              <li><a href='/contactus'>Contact Us</a></li>
-              <li><a href='/signup'>Sign Up</a></li>
+              <li><Link to='/about'>About</Link></li>
+              <li><Link to='/contactus'>Contact Us</Link></li>
+              <li><Link to='/signup'>Sign Up</Link></li>
             </ul>
             <div style={ { marginTop: '5px' } }>
-              <a href='https://www.getapp.com/it-communications-software/a/fenero/reviews/' target='_blank'><img alt='Read Fenero Reviews on GetApp.com' src='https://www.getapp.com/ext/reviews_widget/v1/dark/fenero-application' srcSet='https://www.getapp.com/ext/reviews_widget/v1/dark/fenero-application, https://www.getapp.com/ext/reviews_widget/v1/dark/fenero-application@2x 2x, https://www.getapp.com/ext/reviews_widget/v1/dark/fenero-application@3x 3x' title='Read Qubicles Reviews on GetApp.com' /></a>
+              <a
+                href='https://www.getapp.com/it-communications-software/a/fenero/reviews/'
+                target='_blank'
+              >
+                <img
+                  alt='Read Fenero Reviews on GetApp.com'
+                  src='https://www.getapp.com/ext/reviews_widget/v1/dark/fenero-application'
+                  srcSet='https://www.getapp.com/ext/reviews_widget/v1/dark/fenero-application, https://www.getapp.com/ext/reviews_widget/v1/dark/fenero-application@2x 2x, https://www.getapp.com/ext/reviews_widget/v1/dark/fenero-application@3x 3x'
+                  title='Read Qubicles Reviews on GetApp.com'
+                />
+              </a>
             </div>
           </div>
         </div>
@@ -37,14 +56,20 @@ const Footer = () => (
               <h3>Resources</h3>
             </div>
             <ul className='link-list'>
-              <li><a href='http://support.fenero.com'>Support</a></li>
-              <li><a href='/termsofuse'>Terms of Use</a></li>
-              <li><a href='/privacypolicy'>Privacy Policy</a></li>
+              <li><a to='http://support.fenero.com'>Support</a></li>
+              <li><Link to='/termsofuse'>Terms of Use</Link></li>
+              <li><Link to='/privacypolicy'>Privacy Policy</Link></li>
             </ul>
             <div style={ { marginTop: '5px' } }>
-              <a href='https://www.capterra.com/reviews/161183/Fenero?utm_source=vendor&utm_medium=badge&utm_campaign=capterra_reviews_badge' target='_blank'>
-                {' '}
-                <img border={ 0 } src='https://assets.capterra.com/badge/54c78f6565f4030db187353d12a99c33.png?v=2112709&p=161183' />
+              <a
+                href='https://www.capterra.com/reviews/161183/Fenero?utm_source=vendor&utm_medium=badge&utm_campaign=capterra_reviews_badge'
+                target='_blank'
+              >
+                <img
+                  border={ 0 }
+                  src='https://assets.capterra.com/badge/54c78f6565f4030db187353d12a99c33.png?v=2112709&p=161183'
+                  alt=''
+                />
               </a>
             </div>
           </div>
@@ -52,32 +77,27 @@ const Footer = () => (
         <div className='column'>
           <div className='footer-column'>
             <div className='footer-logo'>
-              <img src='img/logos/qbe-dark.png' alt='' />
+              <img src={ qbeDark } alt='' />
             </div>
             <div className='footer-header'>
               <nav className='level is-mobile'>
                 <div className='level-left level-social'>
                   <a target='_blank' className='level-item' href='https://www.facebook.com/Qubicles.io'>
-                    <span className='icon'><i className='fa fa-facebook' /></span>
+                    <span className='icon'><FacebookIcon /></span>
                   </a>
                   <a target='_blank' className='level-item' href='https://twitter.com/Qubicles'>
-                    <span className='icon'><i className='fa fa-twitter' /></span>
+                    <span className='icon'><TwitterIcon /></span>
                   </a>
                   <a target='_blank' className='level-item' href='https://www.linkedin.com/company/qubiclesio'>
-                    <span className='icon'><i className='fa fa-linkedin' /></span>
-                  </a>
-                  <a target='_blank' className='level-item' href='https://medium.com/@qubicles'>
-                    <span className='icon'><i className='fa fa-medium' /></span>
+                    <span className='icon'><LinkedinIcon /></span>
                   </a>
                 </div>
               </nav>
             </div>
-            <div className='copyright'>
+            <div>
               <span className='moto light-text'>
                 Copyright 2020
-                {' '}
-                <i className='fa fa-copyright' />
-                {' '}
+                <FontAwesomeIcon icon={ faCopyright } className='ml-5 mr-5' />
                 Qubicles, Inc.
               </span>
             </div>
