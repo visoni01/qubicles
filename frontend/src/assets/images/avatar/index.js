91074e93f647ddd2f96816a179edefefe9d910c5
Nitin Verma <nverma@qubicles.io>
2020-07-07 19:27:39 +0530

    Added images for about us page.

---
+++ frontend/src/assets/images/avatar/index.js
@@ -0,0 +1,9 @@
+import sally from './sally.jpg'
+import kareem from './kareem.jpg'
+import terry from './terry.jpg'
+import thomas from './thomas.jpg'
+import carolin from './carolin.png'
+
+export {
+  sally, thomas, carolin, terry, kareem,
+}
