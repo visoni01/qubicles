f89ee22458a5f301c1408e9e09cb3eaad8f283c8
Nitin Verma <nverma@qubicles.io>
2020-07-10 14:06:39 +0530

    Added container for the jobs section.

---
+++ frontend/src/container/People/data.js
@@ -0,0 +1,34 @@
+import {
+  faBell,
+} from '@fortawesome/free-solid-svg-icons'
+
+/**
+ * This is the dummmy data, Which will be removed after the integration of APIs.
+ */
+
+const jobsData = [ {
+  title: 'Announcements',
+  jobs: [
+    {
+      notifications: 43,
+      title: 'Company Annoucements',
+      description: 'Dedicated channel for company annoucements and heads up.',
+      noOfApplications: 49,
+    },
+    {
+      notifications: 43,
+      title: 'Company Annoucements',
+      description: 'Dedicated channel for company annoucements and heads up.',
+      noOfApplications: 49,
+    },
+    {
+      notifications: 43,
+      title: 'Company Annoucements',
+      description: 'Dedicated channel for company annoucements and heads up.',
+      noOfApplications: 49,
+    },
+  ],
+},
+]
+
+export default jobsData
