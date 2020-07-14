56c857fd330d2b5505ea7f0f2d592c7dc08ba797
Nitin Verma <nverma@qubicles.io>
2020-07-14 16:48:53 +0530

    Added dummy data and styles.

--- frontend/src/container/People/data.js
+++ frontend/src/container/People/data.js
@@ -1,7 +1,3 @@
-import {
-  faBell,
-} from '@fortawesome/free-solid-svg-icons'
-
 /**
  * This is the dummmy data, Which will be removed after the integration of APIs.
  */
@@ -31,4 +27,37 @@ const jobsData = [ {
 },
 ]
 
-export default jobsData
+const talentData = [ {
+  title: 'Announcements',
+  users: [
+    {
+      title: 'React Developer',
+      description: 'Front-end Developer',
+      name: 'John',
+      joinedSince: '2 june 2003',
+      rating: 4.5,
+      hourlyRate: 9,
+      courses: 12,
+      awards: 5,
+      jobs: 4,
+      hasPhone: true,
+      hasBackgroundCheck: false,
+    },
+    {
+      title: 'React Developer',
+      description: 'Front-end Developer',
+      name: 'John',
+      joinedSince: '2 june 2003',
+      rating: 4.5,
+      hourlyRate: 9,
+      courses: 12,
+      awards: 5,
+      jobs: 4,
+      hasPhone: true,
+      hasBackgroundCheck: false,
+    },
+  ],
+},
+]
+
+export { jobsData, talentData }
