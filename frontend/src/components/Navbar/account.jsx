3e1a0f5fa916df839e7cd73ab94a8dcb7af05b2d
Nitin Verma <nverma@qubicles.io>
2020-07-19 05:34:42 +0530

    Logout functionality.

---
+++ frontend/src/components/Navbar/account.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Menu, MenuItem, MenuList } from '@material-ui/core'
+import PropTypes from 'prop-types'
+import { Redirect, useHistory } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import './style.scss'
+import User from '../../redux-saga/service/user'
+import { showSuccessMessage, showErrorMessage } from '../../redux-saga/redux/snackbar'
+
+const UserAccount = ({ isOpen, toggleIsOpen }) => {
+  const history = useHistory()
+  const dispatch = useDispatch()
+
+  const logOut = async () => {
+    const responseStatus = await User.logout()
+    if (responseStatus === 200) {
+      history.push('/login')
+      return dispatch(showSuccessMessage({ msg: 'Successfully logged out' }))
+    }
+    return dispatch(showErrorMessage())
+  }
+
+  return (
+  // Temporary logout functionality.
+    <Menu
+      classes={ {
+        paper: 'account-dropdown-list',
+      } }
+      open={ isOpen }
+      onClose={ () => toggleIsOpen(!isOpen) }
+    >
+      <MenuItem onClick={ logOut }>Logout</MenuItem>
+    </Menu>
+  )
+}
+
+UserAccount.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  toggleIsOpen: PropTypes.func.isRequired,
+}
+
+export default UserAccount
