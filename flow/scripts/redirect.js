a37df64e3e241e069079f3904a80d4c1973a059e
Vinayak <visoni@grepruby.io>
2020-07-21 17:19:48 +0530

    Added comment.

--- flow/scripts/redirect.js
+++ flow/scripts/redirect.js
@@ -1,5 +1,10 @@
-// Staging: 
+// Staging: http://staging.qubicles.io/
 // Local: 
+// - http://localhost:3000/ (Frontend)
+// - http://localhost:4000/ (Backend)
+
+// TODO: setup environment variables (on apache side) for the host
+
 const QUBICLE_FRONTEND_URL = "http://staging.qubicles.io/"
 const QUBICLE_BACKEND_URL = "http://staging.qubicles.io:4000/"
 
