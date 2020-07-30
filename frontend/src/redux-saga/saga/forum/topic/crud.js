6186dabc148a32e8848d96e6355c12c5a7ae2a78
Nitin Verma <nverma@qubicles.io>
2020-07-30 21:06:56 +0530

    Refactored code.

--- frontend/src/redux-saga/saga/forum/topic/crud.js
+++ frontend/src/redux-saga/saga/forum/topic/crud.js
@@ -1,12 +1,14 @@
 import { takeLatest, put, select } from 'redux-saga/effects'
 import { updateChannelData } from '../../../redux/actions'
-import { DELETE_TOPIC, LIKE_TOPIC, UNLIKE_TOPIC } from '../../../redux/constants'
+import {
+  DELETE_TOPIC, LIKE_TOPIC, UNLIKE_TOPIC, ADD_TOPIC,
+} from '../../../redux/constants'
 import { getSubstrForNotification } from '../../../../utils/common'
 import { showErrorMessage, showSuccessMessage } from '../../../redux/snackbar'
 import Forum from '../../../service/forum'
 
 function* topicCrudWatcher() {
-  yield takeLatest([ DELETE_TOPIC, LIKE_TOPIC, UNLIKE_TOPIC ], topicCrudWorker)
+  yield takeLatest([ DELETE_TOPIC, LIKE_TOPIC, UNLIKE_TOPIC, ADD_TOPIC ], topicCrudWorker)
 }
 
 function* topicCrudWorker(action) {
@@ -15,18 +17,45 @@ function* topicCrudWorker(action) {
     switch (action.type) {
       case DELETE_TOPIC: {
         const { topicId, topicTitle } = action.payload
-
-        const { channelDetails } = yield select((state) => state.channel)
         yield Forum.deleteTopic({ topicId })
 
         yield put(updateChannelData({
-          data: {
-            channelInfo: channelDetails.channelInfo,
-            channelTopics: channelDetails.channelTopics.filter((topic) => topic.topicId !== topicId),
+          type: DELETE_TOPIC,
+          topicId,
+        }))
+        msg = `Topic '${ getSubstrForNotification(topicTitle) }' has been successfully deleted!`
+        break
+      }
+      case ADD_TOPIC: {
+        const { title, isPublic, channelId } = action.payload
+        const { data } = yield Forum.addNewTopic({
+          title,
+          is_public: isPublic,
+          channel_id: channelId,
+          is_flagged: false, // Temporary set is_flagged false, currently has no functionality.
+        })
+        const { userDetails } = yield select((state) => state.login)
+        const {
+          // eslint-disable-next-line
+          createdAt, updatedAt, topic_id, owner_id, topic_title,
+        } = data
+        yield put(updateChannelData({
+          type: ADD_TOPIC,
+          newTopic: {
+            dateCreatedOn: createdAt,
+            dateLastReplied: '',
+            tags: null,
+            topicId: topic_id,
+            topicOwner: {
+              userId: owner_id,
+              userName: userDetails.full_name, // Also, add user image to owner details.
+            },
+            topicTitle: topic_title,
+            totalReplies: 0,
           },
         }))
 
-        msg = `Topic '${ getSubstrForNotification(topicTitle) }' has been successfully deleted!`
+        msg = `Topic '${ getSubstrForNotification(data.topic_title) }' has been successfully created!`
         break
       }
       case LIKE_TOPIC: {
