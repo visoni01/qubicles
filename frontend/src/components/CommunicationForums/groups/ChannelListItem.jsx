62fe587ee71d789124b3a3461f5c231182de48c9
Nitin Verma <nverma@qubicles.io>
2020-08-10 17:09:37 +0530

    Fix PR comment.

--- frontend/src/components/CommunicationForums/groups/ChannelListItem.jsx
+++ frontend/src/components/CommunicationForums/groups/ChannelListItem.jsx
@@ -1,83 +1,34 @@
 import React, { useState, useCallback } from 'react'
 import PropTypes from 'prop-types'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBell, faEllipsisV, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faBell } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom'
-import { IconButton, Menu, MenuItem } from '@material-ui/core'
-import { useDispatch, useSelector } from 'react-redux'
-import { deleteChannel } from '../../../redux-saga/redux/forum/actions'
-
-const ActionDropdown = ({ anchorEl, handleClose, removeChannel }) => (
-  <div className='category-dropdown'>
-    <Menu
-      classes={ {
-        paper: 'category-dropdown-menu',
-      } }
-      id='menu'
-      anchorEl={ anchorEl }
-      keepMounted
-      open={ Boolean(anchorEl) }
-      onClose={ handleClose }
-    >
-      <MenuItem onClick={ removeChannel }>
-        <FontAwesomeIcon icon={ faTrash } />
-        <span className='remove'>
-          Remove
-        </span>
-      </MenuItem>
-    </Menu>
-  </div>
-)
+import ActionDropdown from './ChannelActions'
 
 const ChannelListItem = ({
   notifications, title, description, noOfTopics, id, categoryId, ownerId,
-}) => {
-  const dispatch = useDispatch()
-  const { userDetails } = useSelector((state) => state.login)
-
-  const [ anchorEl, setAnchorEl ] = useState(null)
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget)
-  }
-
-  const handleClose = () => {
-    setAnchorEl(null)
-  }
-
-  const handelRemoveChannel = () => {
-    dispatch(deleteChannel({ categoryId, channelId: id, title }))
-    handleClose()
-  }
-
-  return (
-    <div className='forum-channel'>
-      <Link to={ `/group/channels/${ id }` } className='channel-link'>
-        <div className='channel-icon'>
-          <FontAwesomeIcon icon={ faBell } />
-          {/* New Topics */}
-          <div className='new-indicator'>
-            <span>{notifications}</span>
-          </div>
+}) => (
+  <div className='forum-channel'>
+    <Link to={ `/group/channels/${ id }` } className='channel-link'>
+      <div className='channel-icon'>
+        <FontAwesomeIcon icon={ faBell } />
+        {/* New Topics */}
+        <div className='new-indicator'>
+          <span>{notifications}</span>
         </div>
-        <div className='channel-meta'>
-          <span>{title}</span>
-          <span>{description}</span>
-        </div>
-        <div className='channel-topics pl-10'>
-          <span>Topics</span>
-          <span>{noOfTopics}</span>
-        </div>
-      </Link>
-      {(ownerId === userDetails.user_id) && (
-      <IconButton onClick={ handleClick }>
-        <FontAwesomeIcon icon={ faEllipsisV } className='is-size-6' />
-      </IconButton>
-      )}
-      <ActionDropdown anchorEl={ anchorEl } handleClose={ handleClose } removeChannel={ handelRemoveChannel } />
-    </div>
-  )
-}
+      </div>
+      <div className='channel-meta'>
+        <span>{title}</span>
+        <span>{description}</span>
+      </div>
+      <div className='channel-topics pl-10'>
+        <span>Topics</span>
+        <span>{noOfTopics}</span>
+      </div>
+    </Link>
+    <ActionDropdown categoryId={ categoryId } channelId={ id } title={ title } ownerId={ ownerId } />
+  </div>
+)
 
 ChannelListItem.defaultProps = {
   notifications: 3,
@@ -93,12 +44,6 @@ ChannelListItem.propTypes = {
   id: PropTypes.number.isRequired,
   categoryId: PropTypes.number.isRequired,
   ownerId: PropTypes.number.isRequired,
-}
-
-ActionDropdown.propTypes = {
-  anchorEl: PropTypes.string.isRequired,
-  handleClose: PropTypes.func.isRequired,
-  removeChannel: PropTypes.func.isRequired,
 }
 
 export default ChannelListItem
