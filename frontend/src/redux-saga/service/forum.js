928df4ea0e211c26c0c5cc3d445646939bc0a5a3
Shivam Jaiswal <sjaiswal@qubicles.io>
2020-08-11 10:41:47 +0530

    Feature: Added like on topic comment

--- frontend/src/redux-saga/service/forum.js
+++ frontend/src/redux-saga/service/forum.js
@@ -109,11 +109,6 @@ class Forum {
     return response
   }
 
-  static async updateCategory(payload) {
-    const response = await apiClient.putRequest(`/forum/categories/${ payload.categoryId }`, payload)
-    return response
-  }
-
   static async updateChannel(payload) {
     const response = await apiClient.putRequest(`/forum/channel/${ payload.channel_id }`, payload)
     return response
