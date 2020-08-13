c39c98c8e69eaadbcb1e4bcb2c5dedec453d7b4c
Vinayak <visoni@grepruby.io>
2020-08-13 12:32:52 +0530

    WIP - Added chips to display image name uploaded

--- frontend/src/components/Dashboard/createPost.jsx
+++ frontend/src/components/Dashboard/createPost.jsx
@@ -3,18 +3,18 @@ import React, {
 } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import {
-  FormControl, Select, InputLabel, MenuItem, makeStyles, Button, Avatar,
+  FormControl, Select, InputLabel, MenuItem, makeStyles, Button, Avatar, Chip,
 } from '@material-ui/core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCamera, faPen, faMinusCircle } from '@fortawesome/free-solid-svg-icons'
+import { faCamera, faPen, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 import { createStatusPostStart } from '../../redux-saga/redux/actions'
-import profileLogo2 from '../../assets/images/helen.jpg'
 import Loader from '../loaders/circularLoader'
 import './style.scss'
 
 const CreatePost = () => {
   const [ postText, setPostText ] = useState(null)
   const [ permission, setPermission ] = useState('public')
+  const [ fileName, setFileName ] = useState(null)
   const fileInput = useRef()
   const dispatch = useDispatch()
 
@@ -24,20 +24,30 @@ const CreatePost = () => {
       file: fileInput.current.files && fileInput.current.files[ 0 ],
       text: postText,
     }
+    debugger
     dispatch(createStatusPostStart(postData))
   }, [ postText, fileInput, permission, dispatch ])
 
   const { isLoading, success } = useSelector((state) => state.createPost)
-
+  const { userDetails } = useSelector((state) => state.login)
   const clear = () => {
     setPostText('')
     setPermission('public')
+    fileInput.current.value = ''
+    setFileName(null)
+  }
+
+  const handleDelete = () => {
+    fileInput.current.value = ''
+    setFileName(null)
   }
 
   useEffect(() => {
     if (success) {
       setPostText('')
       setPermission('public')
+      fileInput.current.value = ''
+      setFileName(null)
     }
   }, [ isLoading, success ])
 
@@ -58,9 +68,16 @@ const CreatePost = () => {
   }))
   const classes = useStyles()
   return (
-    <div className='compose-card is-new-content is-start is-vcenter post-section create-post-status'>
-      <div className='avatar-container'>
-        <Avatar alt='profile-logo-2' src={ profileLogo2 } className='avatar is-hidden-mobile' />
+    <div
+      className='compose-card is-new-content post-item-custom is-start is-vcenter post-section create-post-status'
+      style={ { pointerEvents: isLoading ? 'none' : 'auto' } }
+    >
+      <div className='columns is-vcentered'>
+        <div className='column is-1'>
+          <Avatar className='avatar'>
+            {userDetails && userDetails.full_name && userDetails.full_name[ 0 ].toUpperCase()}
+          </Avatar>
+        </div>
         <div className='status-wrapper column is-11'>
           <textarea
             className='textarea'
@@ -73,6 +90,22 @@ const CreatePost = () => {
         </div>
       </div>
       <div className='columns is-multiline is-full'>
+        <div className='other-options column is-6 is-narrower'>
+          <form onReset={ clear }>
+            <div className='upload-file'>
+              <FontAwesomeIcon icon={ faCamera } />
+              <span className='file-input-label'>Media</span>
+              <input type='file' className='input-field' accept='image/*' ref={ fileInput } onChange={ (event) => setFileName(fileInput.current.files[ 0 ].name) } />
+            </div>
+          </form>
+          {fileName && (
+          <Chip
+            variant='outlined'
+            label={ fileName }
+            onDelete={ handleDelete }
+          />
+          )}
+        </div>
         <div className='column is-6 is-narrower'>
           <FormControl variant='filled' className={ classes.formControl }>
             <InputLabel FormControlClasses={ { focused: classes.customLabel } } id='permission'>Permission</InputLabel>
@@ -91,53 +124,43 @@ const CreatePost = () => {
             </Select>
           </FormControl>
         </div>
-        <div className='other-options column is-6 is-narrower'>
-          <div className='upload-file'>
-            <FontAwesomeIcon icon={ faCamera } />
-            <span className='file-input-label'>Media</span>
-            <input type='file' className='input-field' accept='image/*' ref={ fileInput } />
+        <hr />
+        <div className='columns is-multiline is-vcentered'>
+          <div className='column is-one-third '>
+            <Button
+              variant='contained'
+              className='post-status-button '
+              startIcon={ <FontAwesomeIcon icon={ faPen } /> }
+              onClick={ publish }
+            >
+              publish
+            </Button>
+          </div>
+          <div className='column is-one-third '>
+            { isLoading && (
+            <div>
+              <Loader
+                className='add-status-loader'
+                displayLoaderManually
+                enableOverlay={ false }
+                size={ 40 }
+              />
+            </div>
+            ) }
+          </div>
+          <div className='column is-one-third'>
+            <Button
+              variant='contained'
+              className='post-status-button  '
+              startIcon={ <FontAwesomeIcon icon={ faTrashAlt } /> }
+              onClick={ clear }
+            >
+              clear
+            </Button>
           </div>
         </div>
-        {(fileInput.current) && (
-        <div className='other-options column is-6 is-narrower'>
-          <input value={ fileInput.current } />
-        </div>
-        )}
-        <hr />
-        <div className='column is-6 is-narrower'>
-          <Button
-            variant='contained'
-            className='publish-button '
-            startIcon={ <FontAwesomeIcon icon={ faPen } /> }
-            onClick={ publish }
-          >
-            publish
-          </Button>
-        </div>
-        <div className='column is-6 is-narrower'>
-          <Button
-            variant='contained'
-            className='publish-button '
-            startIcon={ <FontAwesomeIcon icon={ faMinusCircle } /> }
-            onClick={ clear }
-          >
-            clear
-          </Button>
-        </div>
       </div>
-      {
-        isLoading
-        && (
-        <div>
-          <Loader
-            className='add-status-loader'
-            displayLoaderManually
-            enableOverlay={ false }
-            size={ 40 }
-          />
-        </div>
-        )
-      }
+
     </div>
 
   )
