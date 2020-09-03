4a2d05ea00f47076ad7e13e1ad520e8fd43824d5
Shivam Jaiswal <sjaiswal@qubicles.io>
2020-09-03 22:26:24 +0530

    Added style to navbar icons, added link for people page and dashboard

--- frontend/src/components/Navbar/sideBar.jsx
+++ frontend/src/components/Navbar/sideBar.jsx
@@ -11,45 +11,46 @@ import {
   faCompressAlt,
   faQuestionCircle,
 } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 
 const SideBar = () => (
   <nav className='main-menu is-dark side-bar-container'>
     <div className='main-menu-inner'>
       <ul>
-        <li className='side-icon is-active li-custom' data-child-menu='dashboard-menu'>
-          <FontAwesomeIcon icon={ faHome } className='steps-icons' />
-        </li>
+        <Link to='/dashboard' className='side-icon is-active li-custom' data-child-menu='dashboard-menu'>
+          <FontAwesomeIcon icon={ faHome } className='steps-icons icon-hover' />
+        </Link>
         <span className='sub-text'> Floor </span>
         <li className='side-icon li-custom' data-child-menu='documents-menu'>
-          <FontAwesomeIcon icon={ faClock } className='steps-icons' />
+          <FontAwesomeIcon icon={ faClock } className='steps-icons icon-hover' />
         </li>
         <span className='sub-text'> Inbound </span>
         <li className='side-icon li-custom' data-child-menu='business-menu'>
-          <FontAwesomeIcon icon={ faBullhorn } className='steps-icons' />
+          <FontAwesomeIcon icon={ faBullhorn } className='steps-icons icon-hover' />
         </li>
         <span className='sub-text'> Outbound </span>
         <li className='side-icon li-custom' data-child-menu='misc-menu'>
-          <FontAwesomeIcon icon={ faCompressAlt } className='steps-icons' />
+          <FontAwesomeIcon icon={ faCompressAlt } className='steps-icons icon-hover' />
         </li>
         <span className='sub-text'> Omni </span>
-        <li className='side-icon li-custom' data-child-menu='settings-menu'>
-          <FontAwesomeIcon icon={ faUsers } className='steps-icons' />
-        </li>
+        <Link to='/people' className='side-icon li-custom' data-child-menu='settings-menu'>
+          <FontAwesomeIcon icon={ faUsers } className='steps-icons icon-hover' />
+        </Link>
         <span className='sub-text'> People </span>
         <li className='side-icon li-custom' data-child-menu='settings-menu'>
-          <FontAwesomeIcon icon={ faIdCard } className='steps-icons' />
+          <FontAwesomeIcon icon={ faIdCard } className='steps-icons icon-hover' />
         </li>
         <span className='sub-text'> Cen... </span>
         <li className='side-icon li-custom' data-child-menu='settings-menu'>
-          <FontAwesomeIcon icon={ faChartPie } className='steps-icons' />
+          <FontAwesomeIcon icon={ faChartPie } className='steps-icons icon-hover' />
         </li>
         <span className='sub-text'> Reports </span>
         <li className='side-icon li-custom' data-child-menu='settings-menu'>
-          <FontAwesomeIcon icon={ faThLarge } className='steps-icons' />
+          <FontAwesomeIcon icon={ faThLarge } className='steps-icons icon-hover' />
         </li>
         <span className='sub-text'> Apps </span>
         <li className='side-icon li-custom' data-child-menu='settings-menu'>
-          <FontAwesomeIcon icon={ faQuestionCircle } className='steps-icons' />
+          <FontAwesomeIcon icon={ faQuestionCircle } className='steps-icons icon-hover' />
         </li>
         <span className='sub-text'> Help </span>
       </ul>
