95519ed26cc7fad702651114effd1118f031bd0b
Shivam Jaiswal <sjaiswal@qubicles.io>
2020-09-09 18:18:45 +0530

    Feature: Added SendVerificationMail Service for backend

--- backend/app/controllers/auth.controller.js
+++ backend/app/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import VerifyTokenMethod from '../services/user/verifyToken'
+import SendVerificationMailService from '../services/user/sendVerificationMail'
 import HandleCheckrEventService from '../services/authentication/handleCheckrEvents'
 import config from '../../config/app'
 import Responder from '../../server/expressResponder'
@@ -13,6 +14,15 @@ export default class AuthController {
       Responder.success(res, 'User email verified Successfully!!')
     } else {
       Responder.failed(res, verifyTokenResult.errors)
+    }
+  }
+
+  static async sendVerificationMail (req, res) {
+    const sendVerificationMailResult = await SendVerificationMailService.execute(req.body)
+    if (sendVerificationMailResult.successful) {
+      Responder.success(res, 'Verification email sent successfully!')
+    } else {
+      Responder.failed(res, sendVerificationMailResult.errors)
     }
   }
 
