dabc90c74fc155f68f8129eb8c714d39dceb07f7
Nitin Verma <nverma@qubicles.io>
2020-09-18 02:22:58 +0530

    Added svg icons for the navbar.

---
+++ frontend/src/assets/images/icons/navBarIcons/index.js
@@ -0,0 +1,10 @@
+import chatIcon from './chat.svg'
+import walletIcon from './wallet.svg'
+import bellIcon from './notification.svg'
+import homeIcon from './floor.svg'
+import settingIcon from './settings.svg'
+import peopleIcon from './people.svg'
+
+export {
+  chatIcon, walletIcon, bellIcon, homeIcon, settingIcon, peopleIcon,
+}
