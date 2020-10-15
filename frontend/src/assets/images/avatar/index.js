e01a00d5c3bce25dbb27b9940e29e6c1449c81b7
Vinayak <visoni@grepruby.io>
2020-10-15 13:22:55 +0530

    added new avatars

--- frontend/src/assets/images/avatar/index.js
+++ frontend/src/assets/images/avatar/index.js
@@ -3,7 +3,10 @@ import kareem from './kareem.jpg'
 import terry from './terry.jpg'
 import thomas from './thomas.jpg'
 import carolin from './carolin.png'
+import good from './good.jpg'
+import ray from './ray.jpg'
+import helen from './helen.jpg'
 
 export {
-  sally, thomas, carolin, terry, kareem,
+  sally, thomas, carolin, terry, kareem, good, ray, helen,
 }
