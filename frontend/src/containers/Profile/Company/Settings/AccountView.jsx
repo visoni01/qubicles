8aa903c28ff949ff902d0cebd5c7e61c3f69f220
Shivam Jaiswal <sjaiswal@qubicles.io>
2020-12-10 21:27:58 +0530

    Refactor: Added form validation for address and password in settings

--- frontend/src/containers/Profile/Company/Settings/AccountView.jsx
+++ frontend/src/containers/Profile/Company/Settings/AccountView.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable complexity */
-import React, { useState, useCallback, useEffect } from 'react'
+import React, { useCallback } from 'react'
 import {
   Box, Grid, Button, IconButton, Input, Divider, Switch,
 } from '@material-ui/core'
@@ -7,32 +7,25 @@ import PropTypes from 'prop-types'
 import './styles.scss'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEye } from '@fortawesome/free-solid-svg-icons'
-import { useSelector } from 'react-redux'
 import MultiSelectChipItems from '../../../NewPeople/MultiSelectChipItems'
+import { accountSettingInfoPropTypes, accountSettingInfoDefaultProps } from './settingsProps'
 
 export default function AccountView({
-  setOpenDrawer,
+  setOpenDrawer, accountSettingInfo, setAccountSettingInfo,
 }) {
-  const { settings, isLoading } = useSelector((state) => state.companyProfileSettings)
-  const [ accountSettingInfo, setAccountSettingInfo ] = useState(settings)
-
-  useEffect(() => {
-    setAccountSettingInfo(settings)
-  }, [ settings ])
-
   const handleSmsNotificationSwitch = useCallback(() => {
     setAccountSettingInfo((currentSetting) => ({
       ...currentSetting,
       smsNotification: !currentSetting.smsNotification,
     }))
-  }, [])
+  }, [ setAccountSettingInfo ])
 
   const handleEmailNotificationSwitch = useCallback(() => {
     setAccountSettingInfo((currentSetting) => ({
       ...currentSetting,
       emailNotification: !currentSetting.emailNotification,
     }))
-  }, [])
+  }, [ setAccountSettingInfo ])
 
   return (
     <Box className='custom-box'>
@@ -49,11 +42,9 @@ export default function AccountView({
                 <h4 className='h4'>Company ID</h4>
               </div>
               <div className='row-fields'>
-                {!isLoading && (
                 <span className='para'>
                   {accountSettingInfo.companyId}
                 </span>
-                )}
               </div>
             </Grid>
             <Grid item xl={ 6 } lg={ 6 } sm={ 12 } xs={ 12 }>
@@ -69,11 +60,9 @@ export default function AccountView({
                 </Button>
               </div>
               <div className='row-fields'>
-                {!isLoading && (
                 <span className='para'>
                   {accountSettingInfo.companyName}
                 </span>
-                )}
               </div>
             </Grid>
           </Grid>
@@ -95,12 +84,10 @@ export default function AccountView({
                 </Button>
               </div>
               <div className='row-fields flex'>
-                {!isLoading && (
                 <Input
                   defaultValue='****************'
                   className='text-edit'
                 />
-                )}
                 <IconButton>
                   <FontAwesomeIcon icon={ faEye } className='custom-fa-icon light' />
                 </IconButton>
@@ -121,7 +108,6 @@ export default function AccountView({
                 </Button>
               </div>
               <div className='row-fields'>
-                {!isLoading && (
                 <span className='para'>
                   <p>
                     {`${ accountSettingInfo.address }` }
@@ -130,7 +116,6 @@ export default function AccountView({
                     {`${ accountSettingInfo.city } ${ accountSettingInfo.zip }, ${ accountSettingInfo.state } ` }
                   </p>
                 </span>
-                )}
               </div>
             </Grid>
           </Grid>
@@ -152,11 +137,9 @@ export default function AccountView({
                 </Button>
               </div>
               <div className='row-fields'>
-                {!isLoading && (
                 <span className='para'>
                   {accountSettingInfo.email}
                 </span>
-                )}
               </div>
             </Grid>
             <Grid item xl={ 6 } lg={ 6 } sm={ 12 } xs={ 12 }>
@@ -172,11 +155,9 @@ export default function AccountView({
                 </Button>
               </div>
               <div className='row-fields '>
-                {!isLoading && (
                 <span className='para '>
                   {accountSettingInfo.homePhone}
                 </span>
-                )}
               </div>
             </Grid>
           </Grid>
@@ -196,35 +177,29 @@ export default function AccountView({
                 </Button>
               </div>
               <div className='row-fields'>
-                {!isLoading && (
                 <span className='para'>
                   {accountSettingInfo.mobilePhone}
                 </span>
-                )}
               </div>
             </Grid>
             <Grid item xl={ 6 } lg={ 6 } sm={ 12 } xs={ 12 }>
               <div className='row-section small-width'>
                 <h4 className='h4'>SMS Notifications</h4>
-                {!isLoading && (
                 <Switch
                   className='switches'
                   color='primary'
                   checked={ !!accountSettingInfo.smsNotification }
                   onChange={ handleSmsNotificationSwitch }
                 />
-                )}
               </div>
               <div className='row-section small-width'>
                 <h4 className='h4'>Email Notifications</h4>
-                {!isLoading && (
                 <Switch
                   className='switches'
                   color='primary'
                   checked={ !!accountSettingInfo.emailNotification }
                   onChange={ handleEmailNotificationSwitch }
                 />
-                )}
               </div>
             </Grid>
           </Grid>
@@ -245,11 +220,9 @@ export default function AccountView({
                 </Button>
               </div>
               <div className='row-fields '>
-                {!isLoading && (
                 <span className='para'>
                   {accountSettingInfo.website}
                 </span>
-                )}
               </div>
             </Grid>
             <Grid item xl={ 6 } lg={ 6 } sm={ 12 } xs={ 12 }>
@@ -257,9 +230,7 @@ export default function AccountView({
                 <h4 className='h4'>Timezone</h4>
               </div>
               <div className='row-fields no-padding-left'>
-                {!isLoading && (
                 <MultiSelectChipItems />
-                )}
               </div>
             </Grid>
           </Grid>
@@ -271,4 +242,10 @@ export default function AccountView({
 
 AccountView.propTypes = {
   setOpenDrawer: PropTypes.func.isRequired,
+  accountSettingInfo: accountSettingInfoPropTypes,
+  setAccountSettingInfo: PropTypes.func.isRequired,
+}
+
+AccountView.defaultProps = {
+  accountSettingInfo: accountSettingInfoDefaultProps,
 }
