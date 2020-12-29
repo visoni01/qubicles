781948e21942ff7b8401903d4934c949c380b6b2
Rohit Soner <rohit@qubicles.io>
2020-12-29 16:30:18 +0530

    Restructured and refactored Navbar code.

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,4 +1,26 @@
+import React from 'react'
+import { Container } from '@material-ui/core'
+import PropTypes from 'prop-types'
 import SideBar from './menuBar'
 import TopBar from './topBar'
+import './style.scss'
 
-export { SideBar, TopBar }
+const NavBar = ({ children }) => (
+  <div className='navbar-root'>
+    <div className='left-bar'>
+      <SideBar />
+    </div>
+    <div className='right-section'>
+      <TopBar />
+      <Container maxWidth='lg' className='route-component' classes={ { maxWidthLg: 'container-max-width' } }>
+        {children}
+      </Container>
+    </div>
+  </div>
+)
+
+NavBar.propTypes = {
+  children: PropTypes.elementType.isRequired,
+}
+
+export default NavBar
--- frontend/src/routes/customRoutes.jsx
+++ frontend/src/routes/customRoutes.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import {
   Route, Redirect, Switch, useLocation,
 } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import { LinearProgress } from '@material-ui/core'
 import routes from './routeList'
 import { getToken, getUserDetails } from '../utils/common'
+import Navbar from '../components/Navbar'
 
-const CustomRoutes = (routeData) => (
+const suspenseWrapper = (Component) => (
+  <Suspense fallback={ <LinearProgress /> }>
+    <Component />
+  </Suspense>
+)
+
+const CustomRoutes = () => (
   <Switch>
     {
       routes.map(({ path, auth, ...rest }) => (
@@ -38,7 +46,11 @@ const Validator = ({ component: Component, path }) => {
   } else if (userDetails.is_post_signup_completed && path === '/post-signup') {
     component = <Redirect to='/dashboard' />
   } else {
-    component = <Component />
+    component = (
+      <Navbar>
+        {suspenseWrapper(Component)}
+      </Navbar>
+    )
   }
 
   return component
@@ -48,7 +60,9 @@ const Redirector = ({ component: Component, redirectToDashboard }) => {
   if (getToken() && redirectToDashboard) {
     return (<Redirect to='/dashboard' />)
   }
-  return (<Component />)
+  return (
+    suspenseWrapper(Component)
+  )
 }
 
 Validator.propTypes = {
