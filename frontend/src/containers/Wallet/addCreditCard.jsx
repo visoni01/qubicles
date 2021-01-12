1b5e681475041a076a4a51209ee1f7adc1e9fc4a
Vinayak <visoni@grepruby.io>
2021-01-12 16:24:07 +0530

    feature - added paymentMethod, addBankAccount , addCreditCard component

---
+++ frontend/src/containers/Wallet/addCreditCard.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  Dialog, DialogActions, DialogContent,
+  DialogTitle, Button, IconButton, InputBase,
+} from '@material-ui/core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+
+const AddCreditCard = ({
+  open, onClose, onSubmit,
+}) => (
+  <Dialog
+    disableScrollLock
+    open={ open }
+    onClose={ onClose }
+    fullWidth
+    maxWidth='sm'
+    className='custom-modal wallet-root'
+    classes={ { paper: 'wallet-modals' } }
+  >
+    <div className='header'>
+      <DialogTitle>
+        <h2 className='h2'>Add Credit Card</h2>
+      </DialogTitle>
+      <DialogActions className='cross-button'>
+        <IconButton
+          className='is-size-6'
+          onClick={ onClose }
+        >
+          <FontAwesomeIcon className='custom-fa-icon pointer' icon={ faTimes } />
+        </IconButton>
+      </DialogActions>
+    </div>
+    <DialogContent>
+      <h4 className='h4 mt-30'>Credit Card Holder</h4>
+      <div className='mt-10'>
+        <para className='para mt-30'> James Barnett</para>
+      </div>
+      <h4 className='h4 mt-30'>Card Number</h4>
+      <InputBase
+        InputProps={ { inputProps: { min: 0, step: 1 } } }
+        type='number'
+        placeholder='e.g. 100'
+        className='search-input mt-10'
+      />
+      <div className='display-inline-flex justify-between align-items-center is-fullwidth'>
+        <div className='mr-20'>
+          <h4 className='h4 mt-30'>Expiration Date</h4>
+          <InputBase
+            InputProps={ { inputProps: { min: 0, step: 1 } } }
+            type='number'
+            placeholder='e.g. 100'
+            className='search-input mt-10'
+          />
+        </div>
+        <div>
+          <h4 className='h4 mt-30'>CVV</h4>
+          <InputBase
+            InputProps={ { inputProps: { min: 0, step: 1 } } }
+            type='number'
+            placeholder='e.g. 100'
+            className='search-input mt-10'
+          />
+        </div>
+      </div>
+    </DialogContent>
+    <DialogActions className='modal-actions'>
+      <div className='sendQBEModal-buttons is-fullwidth'>
+        <Button
+          disabled
+          className='wide-button'
+          classes={ { root: 'button-primary-small', label: 'button-primary-small-label' } }
+          onClick={ onSubmit }
+        >
+          Add Credit Card
+        </Button>
+        <Button
+          color='secondary'
+          onClick={ onClose }
+          className='cancel-button'
+        >
+          Cancel
+        </Button>
+      </div>
+    </DialogActions>
+  </Dialog>
+)
+
+AddCreditCard.propTypes = {
+  open: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+}
+
+export default AddCreditCard
--- frontend/src/containers/Wallet/paymentCardDetails.jsx
+++ frontend/src/containers/Wallet/paymentCardDetails.jsx
@@ -1,30 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Button } from '@material-ui/core'
 import { paymentCardData } from './testData'
 import RenderPaymentCard from './renderPaymentCard'
+import AddPaymentMethod from './addPaymentMethod'
 
-const PaymentCardDetails = () => (
-  <div>
-    <Box className='custom-box'>
-      <h3 className=' h3 mb-30'> Bank Accounts and Credit Cards </h3>
-      {paymentCardData.map((paymentCard) => (
-        <RenderPaymentCard
-          key={ paymentCard.id }
-          paymentCard={ paymentCard }
-        />
-      ))}
-      <div className='text-center mb-10'>
-        <Button
-          classes={ {
-            root: 'button-primary-text',
-            label: 'button-primary-text-label',
-          } }
-        >
-          Add bank account or credit card
-        </Button>
+const PaymentCardDetails = () => {
+  const [ openAddPaymentMethodModal, setOpenAddPaymentMethodModal ] = useState(false)
+  return (
+    <>
+      <div>
+        <Box className='custom-box'>
+          <h3 className=' h3 mb-30'> Bank Accounts and Credit Cards </h3>
+          {paymentCardData.map((paymentCard) => (
+            <RenderPaymentCard
+              key={ paymentCard.id }
+              paymentCard={ paymentCard }
+            />
+          ))}
+          <div className='text-center mb-10'>
+            <Button
+              classes={ {
+                root: 'button-primary-text',
+                label: 'button-primary-text-label',
+              } }
+              onClick={ () => setOpenAddPaymentMethodModal(true) }
+            >
+              Add bank account or credit card
+            </Button>
+          </div>
+        </Box>
       </div>
-    </Box>
-  </div>
-)
+      <AddPaymentMethod
+        open={ openAddPaymentMethodModal }
+        onClose={ () => setOpenAddPaymentMethodModal(false) }
+      />
+    </>
+  )
+}
 
 export default PaymentCardDetails
