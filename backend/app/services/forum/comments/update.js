04105a8ea83745dfb000c87594cc72abd06763f6
Rohit Soner <rohit@qubicles.io>
2021-01-13 14:42:05 +0530

    db related operations for comment update/delete

---
+++ backend/app/services/forum/comments/update.js
@@ -0,0 +1,41 @@
+import ServiceBase from '../../../common/serviceBase'
+import { updateForumComment, getErrorMessageForService } from '../../helper'
+import logger from '../../../common/logger'
+import { ERRORS } from '../../../utils/errors'
+
+const constraints = {
+  user_id: {
+    presence: { allowEmpty: false }
+  },
+  topic_id: {
+    presence: { allowEmpty: false }
+  },
+  comment: {
+    presence: { allowEmpty: false }
+  }
+}
+
+export class ForumUpdateCommentService extends ServiceBase {
+  get constraints () {
+    return constraints
+  }
+
+  async run () {
+    const { user_id, topic_id, comment } = this.filteredArgs
+    try {
+      const updatedComment = await updateForumComment({
+        topic_id,
+        comment,
+        user_id
+      })
+
+      return {
+        message: 'Forum comment updated Successfully',
+        updatedComment
+      }
+    } catch (err) {
+      logger.error(getErrorMessageForService('ForumUpdateCommentService'), err)
+      this.addError(ERRORS.INTERNAL)
+    }
+  }
+}
