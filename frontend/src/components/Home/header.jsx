c134bb781a70e489201278f263abe814c83c0bc4
Nitin Verma <nverma@qubicles.io>
2021-02-23 22:48:16 +0530

    fix - landing page responsiveness for mobile devices

--- frontend/src/components/Home/header.jsx
+++ frontend/src/components/Home/header.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+/* eslint-disable complexity */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import AccountSettings from '../Navbar/userMenu'
@@ -7,6 +10,7 @@ import ROUTE_PATHS from '../../routes/routesPath'
 
 const Header = () => {
   const { userDetails } = useSelector((state) => state.login)
+  const [ openNavButtons, setOpenNavButtons ] = useState(false)
 
   return (
     <div className='navbar-placeholder'>
@@ -18,46 +22,52 @@ const Header = () => {
               <img className='light-logo logo-height' src='https://i.imgur.com/y5zrESW.png' alt='' />
               <img className='dark-logo logo-height' src='https://i.imgur.com/Ap2GB30.png' alt='' />
             </a>
-            {/* Responsive toggle */}
-            <div className='custom-burger' data-target='is-cloned'>
-              <span className='responsive-btn'>
-                <span className='menu-toggle'>
-                  <span className='icon-box-toggle'>
-                    <span className='rotate'>
-                      <i className='icon-line-top' />
-                      <i className='icon-line-center' />
-                      <i className='icon-line-bottom' />
-                    </span>
-                  </span>
-                </span>
-              </span>
-            </div>
+            <span
+              className={ `navbar-burger burger nav-bar-burger ${ openNavButtons ? 'is-active' : '' }` }
+              onClick={ () => setOpenNavButtons((curr) => !curr) }
+            >
+              <span />
+              <span />
+              <span />
+            </span>
             {/* /Responsive toggle */}
           </div>
           {/* Navbar menu */}
-          <div id='is-cloned' className='navbar-menu'>
+          <div id='is-cloned' className={ `navbar-menu ${ openNavButtons ? 'is-active nav-bar-menu-faded' : '' }` }>
             {/* Navbar Start */}
             <div className='navbar-start'>
               {/* Navbar item */}
-              <Link className='navbar-item is-slide' to='/agents'>
+              <Link
+                className={ `navbar-item is-slide ${ openNavButtons ? 'is-active' : '' }` }
+                to='/agents'
+              >
                 For Agents
               </Link>
               {/* Navbar item */}
-              <Link className='navbar-item is-slide' to='/contactcenters'>
+              <Link
+                className={ `navbar-item is-slide ${ openNavButtons ? 'is-active' : '' }` }
+                to='/contactcenters'
+              >
                 For Contact Centers
               </Link>
               {/* Navbar item */}
-              <Link className='navbar-item is-slide' to='/about'>
+              <Link
+                className={ `navbar-item is-slide ${ openNavButtons ? 'is-active' : '' }` }
+                to='/about'
+              >
                 About
               </Link>
               {/* Navbar item */}
-              <Link className='navbar-item is-slide' to='/contactus'>
+              <Link
+                className={ `navbar-item is-slide ${ openNavButtons ? 'is-active' : '' }` }
+                to='/contactus'
+              >
                 Contact Us
               </Link>
             </div>
             {/* Navbar end */}
             {!(userDetails && userDetails.user_id) ? (
-              <div className='navbar-end'>
+              <div className='navbar-end display-inline-flex justify space-between is-fullwidth padding-15'>
                 {/* Sign up button */}
                 <div className='navbar-item'>
                   <CustomLink
