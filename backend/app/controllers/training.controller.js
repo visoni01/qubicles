6d9f53fc1bb38ce96480dade6f653b97ed1a6896
Nitin Verma <nverma@qubicles.io>
2021-03-30 14:50:13 +0530

    wip- create course API

--- backend/app/controllers/training.controller.js
+++ backend/app/controllers/training.controller.js
@@ -1,11 +1,10 @@
-import {
-  PeopleCreateTrainingCourseService
-} from '../services/people/training'
+import { PeopleAddNewCourseService } from '../services/people/training'
+
 import Responder from '../../server/expressResponder'
 
 export default class TrainingController {
   static async createCourse (req, res) {
-    const course = await PeopleCreateTrainingCourseService.execute({ ...req.query, ...req.body })
+    const course = await PeopleAddNewCourseService.execute({ ...req.body })
     if (course.successful) {
       Responder.success(res, course.result)
     } else {
---
+++ backend/app/services/people/training/index.js
@@ -0,0 +1,1 @@
+export { PeopleAddNewCourseService } from './createCourse'
