e4f9e710101100ca782ebd1700f5c546d7e48779
Nitin Verma <nverma@qubicles.io>
2021-04-26 14:29:23 +0530

    feature - added xQodCourseUnit model

---
+++ backend/app/db/models/xQodCourseUnit.js
@@ -0,0 +1,37 @@
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+  const XQodCourseUnit = sequelize.define('XQodCourseUnit', {
+    unit_id: {
+      type: DataTypes.INTEGER(11),
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    course_id: DataTypes.INTEGER(11),
+    section_id: DataTypes.INTEGER(11),
+    unit_num: DataTypes.STRING(100),
+    title: DataTypes.STRING,
+    details: DataTypes.TEXT,
+    length: DataTypes.INTEGER(11),
+    type: {
+      type: DataTypes.ENUM,
+      values: ['Article', 'Audio', 'Video', 'Test']
+    },
+    order: DataTypes.INTEGER,
+    createdAt: {
+      field: 'created_on',
+      type: DataTypes.DATE
+    },
+    updatedAt: {
+      field: 'updated_on',
+      type: DataTypes.DATE
+    }
+  },
+  {
+    tableName: 'x_qod_course_units',
+    timestamps: true
+  })
+  XQodCourseUnit.associate = function (models) {
+  }
+  return XQodCourseUnit
+}
